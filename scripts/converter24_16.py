f811861053f5dc6fb614e339ed3813b817b28e77
Sasha7b9 <Sasha7b9@tut.by>
2019-03-12 17:31:17 +0300

    *

--- scripts/converter24_16.py
+++ scripts/converter24_16.py
@@ -2,12 +2,14 @@ import os
 from struct import *
 
 #######################################################################
-_symbols = []     # Здесь содержимое файла
-_offset = 0       # Смщещение картинки относительно начала файла
-_numColors = 0    # Количество цветов в картинкe
-_width = 0        # Ширина картинки в пикселях
-_height = 0       # Высота картинки в пикселях
-
+_symbols = []       # Здесь содержимое файла
+_offset = 0         # Смщещение картинки относительно начала файла
+_numColors = 0      # Количество цветов в картинкe
+_width = 0          # Ширина картинки в пикселях
+_height = 0         # Высота картинки в пикселях
+_colors = []        # Цвета изображения. Они идут в таком порядке : цвет промежутков между глифами, цвет пустоты в глифах, цвет символа
+_dX = 0             # Смещение между левыми верхними углами глефов по горизонтали
+_dY = 0             # Смещение между левыми верхними углами глефов по вертикали
 
 
 ########################################################################
@@ -38,13 +40,12 @@ def CalculateOffset():
 
 # Расчёт количества цветов в картинке
 def CalculateNumColors(index):
-    colors = []                             # Список цветов
     while index < len(_symbols):
         color = _symbols[index]
-        if colors.count(color) == 0:
-            colors.append(color)
+        if _colors.count(color) == 0:
+            _colors.append(color)
         index += 1
-    return len(colors)
+    return len(_colors)
 
 # Возвращает ширину изображения в пикселях
 def GetWidthPicture():
@@ -54,9 +55,47 @@ def GetWidthPicture():
 def GetHeightPicture():
     return ReadWord(0x16)
 
+# Возвращает значение пикселя в точке x, y
+def GetPoint(x, y):
+    return _symbols[_offset + x + y * _width]
+
+# Расчёт смещений между глефами
+def CalculateOffsetX():
+    x1 = 0;
+    x2 = 0;
+    for x in range(_width):
+        if GetPoint(x, 149) == _colors[1] or GetPoint(x, 149) == _colors[2]:    # Первое вхождение в глиф
+            x1 = x
+            print("find x1 ", x1)
+            break
+    for x in range(x1, _width):
+        if GetPoint(x, 149) == _colors[0]:    # Вышли за глиф
+            x2 = x
+            print("find first x2 ", x2)
+            break
+    for x in range(x2, _width):
+        if GetPoint(x, 149) == _colors[1] or GetPoint(x, 149) == _colors[2]:    # Вошли в следующий глиф
+            x2 = x
+            print("find second x2 ", x2)
+            break
+    return x2 - x1
+
+def Dump(sizeX, sizeY):
+    for y in range(sizeY):
+        for x in range(sizeX):
+            color = GetPoint(x, y)
+            if color == _colors[0]:
+                print('*', end = '')
+            if color == _colors[1]:
+                print('.', end = '')
+            if color == _colors[2]:
+                print(' ', end = '')
+        print()
 
 
 ###### Start here ######
+
+
 
 _symbols = ReadFile()
 
@@ -73,3 +112,12 @@ print("offset ", _offset)
 print("number colors ", _numColors)
 
 print("size picture ", _width, " x ", _height)
+
+print("colors - ", _colors[0], " ", _colors[1], " ", _colors[2])
+
+_dX = CalculateOffsetX()
+
+print("dX = ", _dX, ", dY = ", _dY)
+
+Dump(70, 70)
+
